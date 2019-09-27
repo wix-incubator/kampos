7716133275dca42240521026969d8727fec65369
Yehonatan Daniv <yehonatand@wix.com>
2019-09-27 14:59:34 +0300

    Revise demo to present 1 example at once

--- demo/index.js
+++ demo/index.js
@@ -38,4 +38,95 @@
             setTimeout(() => document.body.addEventListener('click', navOffHandler, {once: true}), 0);
         });
 
+    function insertSection (id) {
+        const section = document.querySelector(`#${id}`);
+        const main = document.querySelector('main');
+
+        main.innerHTML = section.innerHTML;
+
+        sectionScripts[id]();
+    }
+
+    document.querySelector('nav')
+        .addEventListener('click', e => {
+            const id = e.target.dataset.sectionId;
+            insertSection(id);
+        });
+
+    const sectionScripts = {
+        section1 () {
+            const text1 = document.querySelector('#code1');
+            const preview1 = document.querySelector('#preview1');
+            const videos1 = document.querySelector('#videos1');
+            const refresh1 = document.querySelector('#refresh1');
+
+            fetch('./turbulence.js')
+                .then(resp => resp.text())
+                .then(text => {
+                    text1.value = text;
+                    const doc = CodeMirror.fromTextArea(text1, {
+                        value: text,
+                        lineNumbers: true,
+                        theme: 'dracula'
+                    });
+
+                    const update = () => updatePreview(preview1, doc.getValue(), videos1.innerHTML);
+
+                    refresh1.addEventListener('click', update);
+
+                    update();
+                });
+        },
+
+        section2 () {
+            const text2 = document.querySelector('#code2');
+            const preview2 = document.querySelector('#preview2');
+            const videos2 = document.querySelector('#videos2');
+            const refresh2 = document.querySelector('#refresh2');
+
+            fetch('./hue-fade.js')
+                .then(resp => resp.text())
+                .then(text => {
+                    text2.value = text;
+                    const doc = CodeMirror.fromTextArea(text2, {
+                        value: text,
+                        lineNumbers: true,
+                        theme: 'dracula'
+                    });
+
+                    const update = () => updatePreview(preview2, doc.getValue(), videos2.innerHTML);
+
+                    refresh2.addEventListener('click', update);
+
+                    update();
+                });
+        },
+
+        section3 () {
+            const text3 = document.querySelector('#code3');
+            const preview3 = document.querySelector('#preview3');
+            const videos3 = document.querySelector('#videos3');
+            const refresh3 = document.querySelector('#refresh3');
+
+            fetch('./disp.js')
+                .then(resp => resp.text())
+                .then(text => {
+                    text3.value = text;
+                    const doc = CodeMirror.fromTextArea(text3, {
+                        value: text,
+                        lineNumbers: true,
+                        theme: 'dracula'
+                    });
+
+                    const update = () => updatePreview(preview3, doc.getValue(), videos3.innerHTML);
+
+                    refresh3.addEventListener('click', update);
+
+                    update();
+                });
+        }
+    }
+
+    insertSection('section1');
+    insertSection('section1');
 }());
