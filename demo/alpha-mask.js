edb869388c527efbd01c9adabb4db6c9dcbce388
Vlad Tansky <vl.tansky@gmail.com>
2024-08-21 13:34:57 +0300

    Update demo/alpha-mask.js
    Co-authored-by: Yehonatan Daniv <yehonatand@wix.com>

--- demo/alpha-mask.js
+++ demo/alpha-mask.js
@@ -7,20 +7,22 @@ const target = document.querySelector("#target");
 // create the effects/transitions we need
 const alphaMask = effects.alphaMask();
 
-// init kampos
-const instance = new Kampos({ target, effects: [alphaMask] });
-
 // make sure videos are loaded and playing
 prepareVideos([media1, media2])
-    .then(() => loadImage(`https://picsum.photos/1200/1200?random=1`))
-    .then((img) => {
+    .then(() => {
         const width = media1.videoWidth;
         const height = media1.videoHeight;
+
+        alphaMask.isLuminance = true;
+        alphaMask.mask = media2;
+        // it's a video so update on every frame
+        alphaMask.textures[0].update = true;
+
+        // init kampos
+        const instance = new Kampos({ target, effects: [alphaMask] });
+
         // set media source
         instance.setSource({ media: media1, width, height });
-        alphaMask.isLuminance = true;
-        alphaMask.mask = img; // change to media2 for video as mask
-        alphaMask.textures[0].update = true;
+        // start kampos
+        instance.play();
     });
-// start kampos
-instance.play();
