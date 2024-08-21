c6b373e2e473bb4b3ff6683f5a06385087eb0874
Vlad <vl.tansky@gmail.com>
2024-08-21 16:00:37 +0300

    x

--- demo/blend.js
+++ demo/blend.js
@@ -5,20 +5,21 @@ const media2 = document.querySelector('#video4');
 const target = document.querySelector('#target');
 
 // create the effects/transitions we need
-const alphaMask = effects.alphaMask();
+const blend = effects.blend({
+    mode: 'screen',
+});
 
 // make sure videos are loaded and playing
 prepareVideos([media1, media2]).then(() => {
     const width = media1.videoWidth;
     const height = media1.videoHeight;
 
-    alphaMask.isLuminance = true;
-    alphaMask.mask = media2;
+    blend.image = media2;
     // it's a video so update on every frame
-    alphaMask.textures[0].update = true;
+    blend.textures[0].update = true;
 
     // init kampos
-    const instance = new Kampos({ target, effects: [alphaMask] });
+    const instance = new Kampos({ target, effects: [blend] });
 
     // set media source
     instance.setSource({ media: media1, width, height });
