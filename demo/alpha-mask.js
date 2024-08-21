a39badce0ddc2478bc7d4f00593543b0c63a12be
Vlad <vl.tansky@gmail.com>
2024-08-21 11:27:57 +0300

    fix

--- demo/alpha-mask.js
+++ demo/alpha-mask.js
@@ -1,26 +1,26 @@
-import { Kampos, effects } from '../index.js';
+import { Kampos, effects } from "../index.js";
 
-const media1 = document.querySelector('#video3');
-const media2 = document.querySelector('#video4');
-const target = document.querySelector('#target');
+const media1 = document.querySelector("#video3");
+const media2 = document.querySelector("#video4");
+const target = document.querySelector("#target");
+
 // create the effects/transitions we need
 const alphaMask = effects.alphaMask();
-console.log('works!')
 
 // init kampos
-const instance = new Kampos({target, effects:[alphaMask]});
+const instance = new Kampos({ target, effects: [alphaMask] });
 
 // make sure videos are loaded and playing
 prepareVideos([media1, media2])
-    .then(() => {
+    .then(() => loadImage(`https://picsum.photos/1200/1200?random=1`))
+    .then((img) => {
         const width = media1.videoWidth;
         const height = media1.videoHeight;
-
         // set media source
-        instance.setSource({media: media1, width, height});
-
-        alphaMask.mask = media2;
-
-        // start kampos
-        instance.play();
+        instance.setSource({ media: media1, width, height });
+        alphaMask.isLuminance = true;
+        alphaMask.mask = img; // change to media2 for video as mask
+        alphaMask.textures[0].update = true;
     });
+// start kampos
+instance.play();
