1506f8b60a297b3cf01e2e68bb0238fcf32fdbfc
Vlad <vl.tansky@gmail.com>
2024-08-21 15:41:58 +0300

    chore: prettier

--- dist/index.cjs
+++ dist/index.cjs
@@ -1060,7 +1060,7 @@ float noise (vec3 P) {
     vec3 fade_xyz = fade(Pf0);
     vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
     vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
-    float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+    float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
     return 2.2 * n_xyz;
 }`;
 
@@ -1249,7 +1249,7 @@ vec4 taylorInvSqrt (vec4 r) {
     return 1.79284291400159 - 0.85373472095314 * r;
 }
 
-float noise (vec3 v) { 
+float noise (vec3 v) {
     const vec2 C = vec2(1.0/6.0, 1.0/3.0) ;
     const vec4 D = vec4(0.0, 0.5, 1.0, 2.0);
 
@@ -1272,10 +1272,10 @@ float noise (vec3 v) {
     vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
 
     // Permutations
-    i = mod289(i); 
-    vec4 p = permute( permute( permute( 
+    i = mod289(i);
+    vec4 p = permute( permute( permute(
                  i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
-               + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) 
+               + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
                + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
 
     // Gradients: 7x7 points over a square, mapped onto an octahedron.
@@ -1319,7 +1319,7 @@ float noise (vec3 v) {
     // Mix final noise value
     vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
     m = m * m;
-    return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
+    return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),
                                     dot(p2,x2), dot(p3,x3) ) );
 }`;
 
