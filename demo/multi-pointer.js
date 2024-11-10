23c00e744c78f68654bb5d9a7fdcb34afb273c14
Yehonatan Daniv <maggotfish@gmail.com>
2024-11-10 19:07:25 +0200

    Some params and minor changes to values

--- demo/multi-pointer.js
+++ demo/multi-pointer.js
@@ -5,25 +5,27 @@ import { Kampos, effects } from '../index.js';
  * https://tympanus.net/codrops/2019/10/08/creating-a-water-like-distortion-effect-with-three-js/
  */
 
+const DEBUG = false;
+
 const target = document.querySelector('#target');
 const mapTarget = document.createElement('canvas');
 
-const easeOutSine = (t, b, c, d) => {
-    return c * Math.sin((t / d) * (Math.PI / 2)) + b;
+const easeOutSine = (t) => {
+    return Math.sin(t * Math.PI / 2);
 };
 
-const easeOutQuad = (t, b, c, d) => {
-    t /= d;
-    return -c * t * (t - 2) + b;
+const easeOutQuad = (t) => {
+    return -1 * t * (t - 2) ;
 };
 
 class PointerTexture {
-    constructor ({ target, width, height, radius, intensity, maxAge, canvas }){
+    constructor ({ target, width, height, radius, intensity, maxAge, canvas, forceDecay }){
         this.width = width;
         this.height = height;
-        this.radius = radius;
+        this.radius = radius || 100;
         this.intensity = intensity;
         this.maxAge = maxAge;
+        this.decayFactor = forceDecay || 0.01;
         this.last = null;
         this.points = [];
         this.ctx = canvas.getContext('2d');
@@ -32,7 +34,7 @@ class PointerTexture {
     }
 
     clear() {
-        this.ctx.fillStyle = 'rgba(127, 127, 127, 1)';
+        this.ctx.fillStyle = 'rgba(127, 127, 0, 1)';
         this.ctx.fillRect(0, 0, this.width, this.height);
     }
 
@@ -53,7 +55,7 @@ class PointerTexture {
             point.vx = relatveX / (speed + 1e-5);
             point.vy = relatveY / (speed + 1e-5);
 
-            point.speed = Math.min(speedSquared * 1e3, 1);
+            point.speed = Math.min(speedSquared * 1e4, 1);
         }
 
         this.last = point;
@@ -64,14 +66,18 @@ class PointerTexture {
     update () {
         this.clear();
         this.points.forEach((point, i) => {
-            const decay = 1 - point.age / this.maxAge;
-            const force = point.speed * decay / this.maxAge;
+            const decay = 1 - (point.age / this.maxAge);
+            const force = point.speed * decay * this.decayFactor;
             point.x += point.vx * force;
             point.y += point.vy * force;
             point.age += 1;
 
             if (point.age > this.maxAge) {
                 this.points.splice(i, 1);
+
+                if (this.points.length === 0) {
+                    this.last = null;
+                }
             } else {
                 this.drawPoint(point);
             }
@@ -85,19 +91,14 @@ class PointerTexture {
         };
         let intensity = 1;
         if (point.age < this.maxAge * 0.3) {
-            intensity = easeOutSine(point.age / (this.maxAge * 0.3), 0, 1, 1);
+            intensity = easeOutSine(point.age / (this.maxAge * 0.3));
         } else {
-            intensity = easeOutQuad(
-                1 - (point.age - this.maxAge * 0.3) / (this.maxAge * 0.7),
-                0,
-                1,
-                1
-            );
+            intensity = easeOutQuad(1 - (point.age - this.maxAge * 0.3) / (this.maxAge * 0.7));
         }
         intensity *= point.speed;
 
         const red = (1 - point.vx) / 2 * 255;
-        const green = (point.vy + 1) / 2 * 255;
+        const green = (1 + point.vy) / 2 * 255;
         const blue = intensity * 255;
 
         const offset = this.width * 5;
@@ -115,7 +116,6 @@ class PointerTexture {
 
 const MAP_WIDTH = 1396;
 const MAP_HEIGHT = 992;
-const DEBUG = false;
 
 loadImage(
     `https://picsum.photos/${MAP_WIDTH}/${MAP_HEIGHT}?random=1`,
@@ -135,15 +135,16 @@ loadImage(
         width,
         height,
         canvas: DEBUG ? target : mapTarget,
-        radius: 150,
-        intensity: 1.2,
-        maxAge: 64
+        radius: 130,
+        intensity: 0.6,
+        maxAge: 180,
+        forceDecay: 0.01,
     });
 
     // create the main instance that renders the displaced image
     const displacement = effects.displacement();
     displacement.map = mapTarget;
-    displacement.scale = { x: 0, y: 0};
+    displacement.scale = { x: 0, y: 0 };
     displacement.textures[0].update = true; // to update
 
     if (DEBUG) {
