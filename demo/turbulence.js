31be05c29b8f60a9d35f4bcdc910b9adcc61352f
Yehonatan Daniv <maggotfish@gmail.com>
2025-01-08 22:33:21 +0200

    Added input property to displacement effect

--- demo/turbulence.js
+++ demo/turbulence.js
@@ -1,53 +1,40 @@
 import { Kampos, effects, noise } from '../index.js';
 
-const target = document.createElement('canvas');
-target.width = 854;
-target.height = 480;
+const target = document.querySelector('#target');
+const media = document.querySelector('#video5');
 
-const target2 = document.querySelector('#target');
-const media1 = document.querySelector('#video5');
+prepareVideos([media]).then(() => {
+    const width = media.videoWidth;
 
-// uncomment here if you want to see just the turbulence
-//target2.parentNode.replaceChild(target, target2);
+    // try playing with this factor
+    const AMPLITUDE = 2 / width;
+    const frequency = { x: AMPLITUDE, y: AMPLITUDE };
 
-// try playing with this factor
-const AMPLITUDE = 1 / target.width;
-const frequency = { x: AMPLITUDE, y: AMPLITUDE };
+    // increase/decrease the number of octaves to see different noise patterns
+    const octaves = 4;
+    // change to false (or comment out) if you want to see the turbulence noise variant
+    const isFractal = true;
 
-const octaves = 4;
-// change to false (or comment out) if you want to see the turbulence noise variant
-const isFractal = true;
+    // create the effects we need
+    const turbulence = effects.turbulence({
+        noise: noise.simplex,
+        frequency,
+        octaves,
+        isFractal,
+        output: '', // comment out this line to see the turbulence noise itself
+    });
 
-// create the effects we need
-const turbulence = effects.turbulence({
-    noise: noise.simplex,
-    frequency,
-    octaves,
-    isFractal,
-});
+    // create a simple effect that converts the turbulence return value into the output color
+    const disp = effects.displacement({
+        input: effects.displacement.TURBULENCE,
+        scale: { x: 0.15, y: -0.15 },
+    });
 
-// init kampos
-const instance = new Kampos({ target, effects: [turbulence], noSource: true });
-
-// create a simple effect that converts the turbulence return value into the output color
-const disp = effects.displacement();
-
-const instance2 = new Kampos({ target: target2, effects: [disp] });
-
-// you can increase/decrease the time factor for a faster/slower animation
-instance.play((time) => (turbulence.time = time * 2));
-
-prepareVideos([media1]).then(() => {
-    const width = media1.videoWidth;
-    const height = media1.videoHeight;
+    const instance = new Kampos({ target, effects: [turbulence, disp] });
 
     // set media source
-    instance2.setSource({ media: media1, width, height });
+    instance.setSource(media);
 
-    disp.map = target;
-    disp.scale = { x: 0.15, y: -0.15 };
-    disp.textures[0].update = true; // to update
-
-    // start kampos
-    instance2.play();
+    // you can increase/decrease the time factor for a faster/slower animation
+    instance.play((time) => (turbulence.time = time * 2));
 });
