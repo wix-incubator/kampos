590b71952b034f04c3d2563de1b319635287f3ef
ameerabuf <ameerf@wix.com>
2025-02-09 12:58:33 +0200

    trying to fade out

--- index.umd.js
+++ index.umd.js
@@ -1164,7 +1164,6 @@ const mat3 satmat = mat3(
         offsetInputG = 'u_channelOffsetG',
         offsetInputB = 'u_channelOffsetB',
     } = {}) {
-
         /**
          * @typedef {Object} channelSplitEffect
          * @property {boolean} disabled
@@ -1188,9 +1187,15 @@ const mat3 satmat = mat3(
         vec2 _splitOffsetR = ${offsetInputR};
         vec2 _splitOffsetG = ${offsetInputG};
         vec2 _splitOffsetB = ${offsetInputB};
-        float redSplit = texture2D(u_source, sourceCoord + _splitOffsetR).r;
-        float greenSplit = texture2D(u_source, sourceCoord + _splitOffsetG).g;
-        float blueSplit = texture2D(u_source, sourceCoord + _splitOffsetB).b;
+        vec2 redSample = sourceCoord + _splitOffsetR;
+        vec2 greenSample = sourceCoord + _splitOffsetG;
+        vec2 blueSample = sourceCoord + _splitOffsetB;
+        float redFactor = exp(10.0 * min(min(0.0, min(redSample.x, redSample.y)), min(0.0, min(1.0 - redSample.x, 1.0 - redSample.y))));
+        float greenFactor = exp(10.0 * min(min(0.0, min(greenSample.x, greenSample.y)), min(0.0, min(1.0 - greenSample.x, 1.0 - greenSample.y))));
+        float blueFactor = exp(10.0 * min(min(0.0, min(blueSample.x, blueSample.y)), min(0.0, min(1.0 - blueSample.x, 1.0 - blueSample.y))));
+        float redSplit = texture2D(u_source, sourceCoord + _splitOffsetR).r * redFactor;
+        float greenSplit = texture2D(u_source, sourceCoord + _splitOffsetG).g * greenFactor;
+        float blueSplit = texture2D(u_source, sourceCoord + _splitOffsetB).b * blueFactor;
         color = vec3(redSplit, greenSplit, blueSplit);
     }`,
             },
